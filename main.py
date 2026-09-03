1442cc1ee091b1ec13d1467d180fb011a4c13af2
Alanjydev <alanjy.dev@gmail.com>
2026-09-03 20:09:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-from fastapi import FastAPI
 
-from student import router as student_router
-from teacher import router as teacher_router
-from admin import router as admin_router
-
-app = FastAPI(title="School Management System")
-
-app.include_router(student_router)
-app.include_router(teacher_router)
-app.include_router(admin_router)
-
-
-@app.get("/")
-def home():
-    return {"message": "School Management API"}
